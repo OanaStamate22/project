fd6db9190c87579701136867995c852daa79d5ae
Stamate Oana <oana.stamate@cst-dev.com>
2016-01-14 20:36:38 +0200

    home

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,11 @@
  * Controller of the projectApp
  */
 angular.module('projectApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function ($scope,$location) {
+
+
+    $scope.goToRegister = function(){
+
+      $location.path('/register');
+    };
   });
